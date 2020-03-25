1e9c435665c3b5e1a035a137e60d36f0fe001641
zhangtj <1771988873@qq.com>
2020-03-25 21:54:31 +0800

    1.配置analyse,生成stats.json,使用在线网站分析bundle打包结果 2.安装webpack-bundle-analyzer插件，打包时开启网站打开网页分析bundle打包结果

---
+++ build/webpack.analysis.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.base.js')
+const TerserJSPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = merge(baseConfig, {
+    mode: 'production', //production
+    optimization: { // 这个配置项会把默认的js压缩项给取消掉，所以要配置js压缩
+    	minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+    },
+    // devtool: 'cheap-module-source-map',
+    plugins: [
+    	new webpack.DefinePlugin({
+    		IS_DEV: 'false',
+    		test: '2 + 5',
+    		test2: '"world"'
+    	}),
+        new BundleAnalyzerPlugin()
+    ]
+})
